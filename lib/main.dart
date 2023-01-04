58a8ffc1c83f188c28cbab3327fafcc1efa1d3f8
Nagraj Raikar <raikarnagraj1@gmail.com>
2023-01-04 18:19:17 +0530

    more code adde

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+// void main()=> runApp(MyApp()); shorthand
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app '),
+        ),
+        body: Text('this is my text'),
+      ),
+    );
+  }
+}

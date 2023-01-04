2d69dfcc26e3c440641c1d6ba05e002b46b4ed93
Nagraj Raikar <raikarnagraj1@gmail.com>
2023-01-04 17:27:12 +0530

    complete code removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,1 @@
-// writing clas
 
-class Person {
-  String name = 'max';
-  int age = 30;
-}
-
-int addNumbers(int num1, int num2) {
-  // print(num1 + num2);
-  return num1 + num2;
-}
-
-void main() {
-  var p1 = Person();
-  print(p1.age);
-  addNumbers(1, 2);
-  print("hello");
-}

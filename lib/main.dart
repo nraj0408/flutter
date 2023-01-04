34b133843e5363d7fed1651dc82802e78216c1fc
Nagraj Raikar <raikarnagraj1@gmail.com>
2023-01-04 18:40:01 +0530

    added button

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'what\'s is your favorite color ?',
+      'what\'s your fovaorite animal ?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app '),
         ),
-        body: Text('this is my text'),
+        body: Column(
+          children: [
+            Text("the question"),
+            RaisedButton(),
+          ],
+        ),
       ),
     );
   }
